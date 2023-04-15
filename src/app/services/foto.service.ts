617d68b59494d478a1762ef263fac11cdf6dfac8
jhandiaz30 <jmauricio468@gmail.com>
2023-04-15 03:51:18 +0200

    commit

--- src/app/services/foto.service.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from "@angular/core";
-import { Camera, CameraOptions } from "@ionic-native/camera/ngx";
-
-@Injectable({
-  providedIn: "root",
-})
-export class FotoService {
-  constructor(private camera: Camera) {}
-}
--- src/environments/environment.prod.ts
+++
@@ -1,6 +0,0 @@
-//URL HACIA LA API
-
-export const environment = {
-  production: true,
-  api: 'http://192.168.1.90:83',
-};
